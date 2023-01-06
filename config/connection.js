1ff3887a1d4b8e3c5c5d123d59c75c4af94479ca
Ameen-vv <mohammedameenvv@gmail.com>
2023-01-06 11:00:16 +0530

    DB to env

--- config/connection.js
+++ config/connection.js
@@ -3,5 +3,5 @@ const User = require('../model/userSchema')
 
 
 module.exports.connect=function(){
-    mongoose.connect('mongodb+srv://Elearn:elearn123@cluster0.futjdlg.mongodb.net/Cluster0?retryWrites=true&w=majority',()=>console.log('connected to database'))
+    mongoose.connect(process.env.DB_LINK,()=>console.log('connected to database'))
 }
