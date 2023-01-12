53ba73bccee4e60bc97b2eb974cd4df09266f0e1
Ameen-vv <mohammedameenvv@gmail.com>
2023-01-12 19:33:04 +0530

    error

--- app.js
+++ app.js
@@ -57,7 +57,24 @@ app.use(multer({storage:fileStorage}).fields([{name:'image',maxCount:1},{name:'v
 
 app.use('/admin',adminRouter)
 app.use('/',userRouter)
+app.use(function(req, res, next) {
+  res.status(404);
 
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('Error');
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.json({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+});
 
 
 
